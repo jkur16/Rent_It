d93e8dd723e1c076b0f81583a54aa3477b74472f
CadenWit <cadenwitbeck@gmail.com>
2021-07-19 13:55:28 -0400

    added validator

--- public/js/SignUp.js
+++ public/js/SignUp.js
@@ -1,25 +1,25 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const username = document.querySelector('.username-link').value.trim();
-    const email = document.querySelector('.email-link').value.trim();
-    const password = document.querySelector('.password-link').value.trim();
-  
-    if (username && email && password) {
-      const response = await fetch('/api/users/signup', {
-        method: 'POST',
-        body: JSON.stringify({ username, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/signedin');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const username = document.querySelector(".username-link").value.trim();
+  const email = document.querySelector(".email-link").value.trim();
+  const password = document.querySelector(".password-link").value.trim();
+
+  validator.isEmail("foo@bar.com"); //=> true
+
+  if (username && email && password) {
+    const response = await fetch("/api/users/signup", {
+      method: "POST",
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/signedin");
+    } else {
+      alert(response.statusText);
     }
-  };
-  
-  document
-    .querySelector('#signup')
-    .addEventListener('submit', signupFormHandler);
+  }
+};
+
+document.querySelector("#signup").addEventListener("submit", signupFormHandler);
